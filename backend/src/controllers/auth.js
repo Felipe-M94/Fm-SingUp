baded1ade3d225ccdc6578340a9053566c894dad
Felipe-M94 <felipe.silva.mendonca@gmail.com>
2020-10-19 21:17:57 -0300

    auth jwt

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const authConfig = require('../config/auth.json')
 
 const User = require('../models/user')
 
 const router = express.Router()
+
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  })
+}
 
 router.post('/register', async (request, response) => {
   const { email } = request.body
@@ -15,10 +25,32 @@ router.post('/register', async (request, response) => {
 
     user.password = undefined
 
-    return response.send({ user })
+    return response.send({ 
+      user,
+      token: generateToken({ id: user.id }),
+    })
   } catch (err) {
     return response.status(400).send({ error: 'Falha ao registrar usuário!' })
   }
+})
+
+router.post('/authenticate', async (request, response) => {
+  const { email, password } = request.body
+
+  const user = await User.findOne({ email }).select('+password')
+
+  if (!user)
+    return response.status(400).send({ error: 'Usuário não existe' })
+
+  if (!await bcrypt.compare(password, user.password))
+    return response.status(400).send({ error: 'Senha invalida' })
+
+  user.password = undefined
+
+  response.send({ 
+    user, 
+    token: generateToken({ id: user.id }),
+  })
 })
 
 module.exports = app => app.use('/auth', router)
