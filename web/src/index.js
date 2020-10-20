3feaf5ebbe38e1bba426597cf5a8e6b442add439
Felipe-M94 <felipe.silva.mendonca@gmail.com>
2020-10-20 09:42:12 -0300

    format

---
+++ web/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import SingUp from './components/SingUp'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SingUp />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
