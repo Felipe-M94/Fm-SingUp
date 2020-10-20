c4ed9a001df6f40a3d6b38e0438d480ade10e0e6
Felipe-M94 <felipe.silva.mendonca@gmail.com>
2020-10-20 16:33:49 -0300

    uupp

--- web/src/components/SingUp.js
+++ web/src/components/SingUp.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import api from '../services/api'
+
 import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
 import CssBaseline from '@material-ui/core/CssBaseline'
